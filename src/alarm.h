0375918c0f07b9247794823ac66fa13ef4f4b6dd
Henrique Nunes <hrn2001@gmail.com>
2021-11-24 19:06:08 +0000

    Comments on alarm.h and log.h done

--- src/alarm.h
+++ src/alarm.h
@@ -2,6 +2,14 @@
 
 #include "macros.h"
 
-
+/**
+ * @brief Handle when an alarm signal is received
+ * 
+ */
 void alarm_handler();
+/**
+ * @brief Set alarm's signal behaviour
+ * 
+ * @return int 0 on success, -1 on failure
+ */
 int set_alarm();
--- src/log.h
+++ src/log.h
@@ -1,9 +1,27 @@
 #pragma once
-
+/**
+ * @brief Print a message for interesting content
+ * 
+ * @param s Personalized text
+ */
 void log_msg(char* s);
-
+/**
+ * @brief Print a message for an error occured
+ * 
+ * @param s Personalized text
+ */
 void log_err(char* s);
-
+/**
+ * @brief Print a message for a frame transmitted
+ * 
+ * @param type Personalized text
+ * @param n -1 when not necessary otherwise add the number to the message
+ */
 void log_sent(char* type, int n);
-
+/**
+ * @brief Print a message for a frame received
+ * 
+ * @param type Personalized text
+ * @param n -1 when not necessary otherwise add the number to the message
+ */
 void log_rcvd(char* type, int n);
